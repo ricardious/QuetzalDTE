505ca5551fb39019f0b4a6264a27c68159e0ccbe
ricardious <castaneda.systems@gmail.com>
2025-05-06 19:15:13 -0600

    feat(settings): add STATIC_ROOT configuration for static files

--- client/quetzaldte/settings.py
+++ client/quetzaldte/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
