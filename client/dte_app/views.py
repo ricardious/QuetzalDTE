897cb874b3c976f9f2599b92183da05a770e20bf
ricardious <castaneda.systems@gmail.com>
2025-05-08 00:01:03 -0600

    feat(views): add views for home, dashboard, file upload, query data, and VAT summary

--- client/dte_app/views.py
+++ client/dte_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import os
+import tempfile
+from datetime import date
 
-# Create your views here.
+GLOBAL_CONTEXT = {"file_content": None, "binary_file": None, "file_name": None}
+ENDPOINT = "http://localhost:4000/"
+
+def home(request):
+    return render(request, 'pages/home.html', {
+        'today': date.today().strftime('%d/%m/%Y'),
+    })
+
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
+
+def upload_file(request):
+    return render(request, 'pages/upload_file.html')
+
+def query_data(request):
+    return render(request, 'pages/query_data.html')
+
+def vat_summary(request):
+    return render(request, 'pages/vat_summary.html')
